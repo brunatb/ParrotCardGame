f91dae43e80cd51773ee6ecd3b5529600e0d483b
Bruna Topan <brutopanb@gmail.com>
2021-08-01 17:57:35 -0300

    Ranking implemented

--- scripts/script.js
+++ scripts/script.js
@@ -7,8 +7,20 @@ function startGame(){
     numberOfCards = prompt("Com quantas cartas você gostaria de jogar?");
   }
 
+  getPlayerInfo(numberOfCards);
   cardsDistribution(numberOfCards);
+
   clockId = setInterval(incrementClock, 1000);
+}
+
+function getPlayerInfo(numberOfCards) {
+  let name = prompt("Qual o seu nome?");
+
+  while(!name) {
+    name = prompt("Qual o seu nome?");
+  }
+
+  playersRanking.push({name, numberOfCards, time: 0});
 }
 
 function cardsDistribution(numberOfCards) {
@@ -28,8 +40,10 @@ function cardsDistribution(numberOfCards) {
 function showCards(selectedParrots) {
   const upRow = document.querySelector(".up-row");
   const downRow = document.querySelector(".down-row");
+
   upRow.innerHTML = "";
   downRow.innerHTML = "";
+
   for(let i = 0; i < selectedParrots.length/2; i++) {
     upRow.innerHTML+= `
       <li class="card" onclick="turnCard(this)">
@@ -70,7 +84,9 @@ function turnCard(card, check=true) {
 
 function checkCards() {
   const turnedCards = document.querySelectorAll(".turned");
+
   numberOfMoves++;
+  
   if(turnedCards.length === 1){
     turnedCards[0].setAttribute("onclick","");
     return;
@@ -131,17 +147,79 @@ function finishGame(){
   const clock = document.querySelector(".clock span");
   alert(`Você ganhou em ${numberOfMoves} jogadas e ${clock.innerHTML} segundos`);
 
-  const answer = prompt("Gostaria de jogar novamente?");
+  playersRanking[playersRanking.length - 1].time = parseInt(clock.innerHTML);
 
-  if(answer === 'sim'){
-    clock.innerHTML = '0';
-    numberOfMoves = 0;
-    startGame();
+  showRanking();
+}
+
+function showRanking() {
+  const playersToShow = [];
+  const ranking = document.querySelector(".hidden");
+  const title = ranking.querySelector("h1");
+
+  title.innerHTML = `Melhores Jogadores - ${playersRanking[playersRanking.length-1].numberOfCards} cartas`
+
+  for(let i = 0; i < playersRanking.length; i++){
+    if(playersRanking[i].numberOfCards === playersRanking[playersRanking.length-1].numberOfCards){
+      playersToShow.push(playersRanking[i]);
+    }
   }
+
+  playersToShow.sort(order);
+
+  const table = document.querySelector("table");
+  table.innerHTML = `
+  <tr class="table-top">
+    <th>Classificação</th>
+    <th>Nome</th>
+    <th>Quantidade de cartas</th>
+    <th>Tempo</th>
+  </tr>`;
+  
+  for(let i = 0; i < playersToShow.length; i++){
+    table.innerHTML += `
+    <tr>
+      <td>${i + 1}</td>
+      <td>${playersToShow[i].name}</td>
+      <td>${playersToShow[i].numberOfCards}</td>
+      <td>${playersToShow[i].time} segundos</td>
+    </tr>
+    `
+  }
+  
+  ranking.classList.add("ranking");
+  ranking.classList.remove("hidden");
+
+}
+
+function restartGame() {
+  const clock = document.querySelector(".clock span");
+  const ranking = document.querySelector(".ranking");
+  ranking.classList.add("hidden");
+  ranking.classList.remove("ranking");
+
+  clock.innerHTML = '0';
+  numberOfMoves = 0;
+  startGame();
+}
+
+function closeRanking() {
+  const ranking = document.querySelector(".ranking");
+  ranking.classList.remove("ranking");
+  ranking.classList.add("hidden");
 }
 
 function comparador() { 
 	return Math.random() - 0.5; 
+}
+
+function order(a, b) {
+  if(a.time > b.time) {
+    return 1;
+  }else if(a.time < b.time){
+    return -1;
+  }
+  return 0;
 }
 
 function incrementClock() {
